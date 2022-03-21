0a3c3469b50fb3f53e194fc52c3d7d806edceac4
EdgarCortez1 <ea.cortezz@gmail.com>
2022-03-21 14:18:16 -0500

    Added calls to get all items or a singular item

--- DotNet_5_REST_API/Catalog/Controllers/ItemsController.cs
+++ DotNet_5_REST_API/Catalog/Controllers/ItemsController.cs
@@ -1,12 +1,43 @@
+using System;
+using System.Collections.Generic;
+using Catalog.Entities;
 using Catalog.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.Controllers
 {
+    
     [ApiController]
     [Route("items")]
     public class ItemsController : ControllerBase
     {
         private readonly InMemItems_Repository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItems_Repository();
+        }
+
+        // GET /Items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+          var items = repository.GetItems();
+          return items;
+        }
+
+        // GET /items/id
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
     }
 }
